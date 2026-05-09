139761efde85184b3a0888836f24ab5c0fb6f8d7
abdulloh8085 <abdulloh.im4@gmail.com>
2026-05-09 17:59:46 +0900

    Feat: learn string in Python

---
+++ primitive.py
@@ -0,0 +1,25 @@
+print("===== number =====")
+
+# in JAVA, variable is a name of storage location!
+# but in Python, variable is named reference!
+
+count = 100
+count_type = type(count)
+# print("count", count, count_type)
+print(f"the count: {count} and type: {count_type}")
+
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(result1, result2)
+
+print("===== string =====")
+# METHODS: upper() lower() title()  find() replace()
+
+course = "AI Python FullStack"
+result3 = type(course)
+print(f"the result (1): {result3}")
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
