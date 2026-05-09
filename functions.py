e7c85136c8a24d6ae47565d62dcb9f44f326e95a
abdulloh8085 <abdulloh.im4@gmail.com>
2026-05-09 18:48:30 +0900

    Feat: fix: learn keyword & default arguments

--- functions.py
+++ functions.py
@@ -32,3 +32,22 @@ print("result1:", result1)
 
 result2 = greeting("Justin")
 print("result2:", result2)
+
+print("=====  Keyword & Default arguments =====")
+
+# DEFINE
+
+
+def give_greet(name, age=22):
+    print("give_greet is executed")
+    return f"Hi {name}, you are only {age} years old!"
+
+# CALL
+
+
+result3 = give_greet(name="Justin", age=28)
+print("result3:", result3)
+
+# Default argument
+result4 = give_greet("John",)
+print("result4:", result4)
