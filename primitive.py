3d213be7b37e06b61d7a35a684bf10652b14e5cf
abdulloh8085 <abdulloh.im4@gmail.com>
2026-05-09 18:14:07 +0900

    Feat: learn Python boolean

--- primitive.py
+++ primitive.py
@@ -23,3 +23,26 @@ print(f"the result (2): {result}")
 
 result = course.upper()
 print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result (4): {result}")
+
+print("===== boolean =====")
+
+# functions > type() input() bool() int() str()
+
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTH vs FALSY value
+# TRUTH > True 100 -100 "MIT"
+# FALSY > False 0 "" None
+
+test_falsy = "" or False or None or 0 or 100
+print("Test_falsy :", bool(test_falsy))
+
+test_falsy = "MIT"
+print("Test_truthy:", bool(test_falsy))
